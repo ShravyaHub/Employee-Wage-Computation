d173fc38271026e426e24555f9417b0105ed32d1
ShravyaHub <shravyak710@gmail.com>
2020-11-28 19:22:09 +0530

    ADDED: code to calculate employee's wage for a month with twenty working days

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -3,6 +3,8 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUMBER_OF_WORKING_DAYS = 20;
+
 function getWorkingHours(employeeCheck) {
     switch(employeeCheck) {
         case IS_PART_TIME:
@@ -13,8 +15,14 @@ function getWorkingHours(employeeCheck) {
             return 0;
     }
 }
-let employeeHours = 0;
-let employeeCheck = Math.floor(Math.random() * 10) % 3;
-employeeHours = getWorkingHours(employeeCheck);
-let employeeWage = employeeHours * WAGE_PER_HOUR;
-console.log("Employee hours = " + employeeHours + ", employee wage = " + employeeWage);
+
+let totalEmployeeHours = 0;
+
+for(let day = 0; day < NUMBER_OF_WORKING_DAYS; day++) {
+    let employeeCheck = Math.floor(Math.random() * 10) % 3;
+    totalEmployeeHours += getWorkingHours(employeeCheck);
+}
+
+let employeeWage = totalEmployeeHours * WAGE_PER_HOUR;
+console.log("Employee hours = " + totalEmployeeHours + ", employee wage = " + employeeWage);
+
