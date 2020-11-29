1a13fb3838b10448506dad664de535fe37882af9
ShravyaHub <shravyak710@gmail.com>
2020-11-29 19:53:45 +0530

    ADDED: code to validate ID, gender and salary

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -1,7 +1,4 @@
 class EmployeePayrollData {
-    salary;
-    gender;
-    startDate;
 
     constructor(...params) {
         this.id = params[0];
@@ -9,6 +6,17 @@ class EmployeePayrollData {
         this.salary = params[2];
         this.gender = params[3];
         this.startDate = params[4];
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    set id(id) {
+        if(id > 0)
+            this._id = id;
+        else
+            throw "Invalid ID";
     }
 
     get name() {
@@ -23,6 +31,29 @@ class EmployeePayrollData {
             throw "Invalid name";
     }
 
+    get salary() {
+        return this._salary;
+    }
+
+    set salary(salary) {
+        if(salary > 0)
+            this._salary = salary;
+        else
+            throw "Invalid salary";
+    }
+
+    get gender() {
+        return this._gender;
+    }
+
+    set gender(gender) {
+        let genderRegex = /^(F|M)$/;
+        if(genderRegex.test(gender))
+            this._gender = gender;
+        else
+            throw "Invalid gender";
+    }
+
     toString() {
         const options = { year: 'numeric', month: 'long', day: 'numeric'};
         const employeeDate = this.startDate == undefined ? "undefined": this.startDate.toLocaleDateString("en-US", options);
@@ -31,14 +62,17 @@ class EmployeePayrollData {
 }
 
 try {
-let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000);
-console.log(employeePayrollData.toString());
-employeePayrollData.id = 2;
-employeePayrollData.name = "Mark";
-console.log(employeePayrollData.toString());
+    let employeePayrollData = new EmployeePayrollData(1, "Frank", 23000, "M", new Date());
+    console.log(employeePayrollData.toString());
+    employeePayrollData.id = 2;
+    employeePayrollData.name = "Mark";
+    employeePayrollData.salary = 20000;
+    employeePayrollData.gender = "M";
+    employeePayrollData.startDate = new Date();
+    console.log(employeePayrollData.toString());
 
-let newEmployeePayrollData = new EmployeePayrollData(3, "Shravya", 40000, "F", new Date());
-console.log(newEmployeePayrollData.toString());
+    let newEmployeePayrollData = new EmployeePayrollData(1, "Shravya", 40000, "F", new Date());
+    console.log(newEmployeePayrollData.toString());
 } catch(exception) {
     console.error(exception)
 }
